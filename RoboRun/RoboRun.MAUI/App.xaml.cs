a83474d44571e6a2bdcc74e50e2540200bf33d78
Zoltan Orosz <zoltan.orosz@leopoly.com>
2022-12-11 15:24:43 +0100

    Add existing Persistence project

--- RoboRun/RoboRun.MAUI/App.xaml.cs
+++ RoboRun/RoboRun.MAUI/App.xaml.cs
@@ -1,11 +1,36 @@
-﻿namespace RoboRun.MAUI;
+﻿using Java.Security.Cert;
+using RoboRun.Model;
+using RoboRun.Persistence;
+using RoboRun.ViewModel;
+
+namespace RoboRun.MAUI;
 
 public partial class App : Application
 {
-	public App()
+	#region Private constants
+
+	private const string SuspendedGameSavePath = "SuspendedGame";
+
+	#endregion
+
+	#region Private fields
+
+	private readonly AppShell _appShell;
+	private readonly IRoboRunDataAccess _roboRunDataAccess;
+	private readonly RoboRunModel _roboRunModel;
+	private readonly IStore _roboRunStore;
+	private readonly RoboRunViewModel _roboRunViewModel;
+
+    #endregion
+
+    #region Constructor
+
+    public App()
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		_roboRunStore = new RoboRunStore();
 	}
+
+    #endregion
 }
