88de615ec828c073ad254b80a409240e7ce8403d
Zoltan Orosz <zoltan.orosz@leopoly.com>
2022-12-11 15:40:24 +0100

    Add Store classes to Persistence

---
+++ RoboRun/RoboRun.MAUI/Persistence/RoboRunStore.cs
@@ -0,0 +1,20 @@
+﻿namespace RoboRun.Persistence
+{
+    public class RoboRunStore : IStore
+    {
+        public async Task<IEnumerable<string>> GetFilesAsync()
+        {
+            return await Task.Run(() => Directory.GetFiles(FileSystem.AppDataDirectory)
+                                                 .Select(Path.GetFileName)
+                                                 .Where(name => name?.EndsWith(".stl") ?? false)
+                                                 .OfType<string>());
+        }
+
+        public async Task<DateTime> GetModifiedTimeAsync(string name)
+        {
+            var info = new FileInfo(Path.Combine(FileSystem.AppDataDirectory, name));
+
+            return await Task.Run(() => info.LastWriteTime);
+        }
+    }
+}
